0d00e98db096950ccd64c8e221f8cbf9896a21c6
doyun0304 <ddyy0304@gmail.com>
2023-07-06 23:29:27 +0900

    Add global library, some add

---
+++ src/Game.java
@@ -0,0 +1,12 @@
+import global.Vec2D;
+
+public class Game {
+    int difficulty;
+    int gameWidth;
+    int gameHeight;
+    public Game(){
+        difficulty = 0;
+        gameWidth = 7;
+        gameHeight = 7;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
+    private static Game game;
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Match Three Game");
+        initialize();
+        makeGUI();
+    }
+
+    public static void initialize(){
+        game = new Game();
+    }
+
+    public static void makeGUI() {
+
     }
 }
